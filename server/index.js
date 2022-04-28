0fe91d477246f5b92d5c3383937ecc55ff7c8092
Allen Mounivong <allenmounivong@gmail.com>
2022-04-28 15:56:59 -0500

    second commit

--- server/index.js
+++ server/index.js
@@ -3,6 +3,17 @@ const path = require('path');
 
 const app = express();
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require("rollbar");
+var rollbar = new Rollbar({
+  accessToken: '2c9ab3cd513246e68f286fdb75be78ef',
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
+
+// record a generic message and send it to Rollbar
+rollbar.log("Hello world!");
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'));
 });
